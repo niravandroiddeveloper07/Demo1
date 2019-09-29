3bea1b6ef0ebbac873b3ae6deaffe0940d6d846d
Nirav <niravdhanani07@gmail.com>
2019-09-29 20:28:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/network/ApiClient.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+object ApiClient {
+
+    val BASE_URL = "http://api.themoviedb.org/3/"
+    private var retrofit: Retrofit? = null
+
+    val client: Retrofit?
+        get() {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(getOkHttpClient())
+                    .build()
+            }
+            return retrofit
+        }
+
+
+    fun getOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.HEADERS
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(interceptor)
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .addInterceptor(SupportInterceptor())
+            .authenticator(SupportInterceptor())
+        return builder.build()
+    }
+}
